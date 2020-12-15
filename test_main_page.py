92f1d0316537b0711746ab8571f2aad669a71487
denis <yucant3@gmail.com>
2020-12-15 19:26:34 +0300

    edit main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,7 @@
 
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+
 
 
 def test_guest_can_go_to_login_page(browser):
