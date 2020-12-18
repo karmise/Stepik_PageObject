e4fb565690dc8bddec6f870b8dfe754c5df64d3e
denis <yucant3@gmail.com>
2020-12-18 19:56:30 +0300

    not ready yet

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
-from base_page import BasePage
 from locators import LoginPageLocators
+from .base_page import BasePage
 
 
 class LoginPage(BasePage):
@@ -9,13 +9,14 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
+        assert self.is_element_present(*LoginPageLocators.LOGIN_URL), "Login url is not presented"
+        assert "login" in self.browser.current_url, "There is not login in url"
         assert True
 
     def should_be_login_form(self):
-        self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
         assert True
 
     def should_be_register_form(self):
-        self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
         assert True
