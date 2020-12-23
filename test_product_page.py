cb0e74021ded0ca62d7dfe8681ff45b4a3482c38
denis <yucant3@gmail.com>
2020-12-23 19:08:31 +0300

    add parametrize to test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,15 +1,17 @@
 from pages.product_page import ProductPage
+import pytest
 import time
 
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
 
-basket_page = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 
-
-def test_guest_can_add_product_to_basket(browser):
-    page = ProductPage(browser, basket_page)
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.basket_should_be()
-    time.sleep(5)
+    #time.sleep(5)
     pass
